b4639ea52ac9c3b803ba363719fbf20ef3e6ceb0
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-06 07:45:34 -0700

    Refactor users create action (incomplete)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,12 +12,13 @@ class UsersController < ApplicationController
       # It would probably be more efficient to have a post endpoint /users/:email so we don't have an additional handshake here
       user_info = service.get_url("/users/#{new_user.email}")
       service.post_url("/users/#{user_info[:id]}", user_params)
+      session[:user_id] = user_info[:id]
       flash[:success] = "Welcome, #{new_user.name}!"
       redirect_to "/rounds/public"
-    # else  
+    # else
     #   flash[:error] = new_user.errors.full_messages.to_sentence
     #   redirect_to "/users/new"
-    end 
+    end
   end
 
   def login_form; end
