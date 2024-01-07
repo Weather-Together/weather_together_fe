14c302e44ca5903e72ef0201c5487eed13faa71a
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-07 14:22:15 -0700

    Clean up controllers

--- app/controllers/rounds/votes_controller.rb
+++ app/controllers/rounds/votes_controller.rb
@@ -1,10 +1,6 @@
 class Rounds::VotesController < ApplicationController
   def index
-    @previous_rounds_info = get_previous_rounds
-  end
-
-  def get_previous_rounds
     service = UsersService.new
-    service.previous_rounds
+    @previous_rounds_info = service.previous_rounds
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @previous_rounds_info = get_previous_rounds
+    service = UsersService.new
+    @previous_rounds_info = service.previous_rounds
   end
 
   def login
@@ -14,39 +15,4 @@ class UsersController < ApplicationController
     session[:user_id] = nil
     redirect_to root_path
   end
-
-  def get_previous_rounds
-    service = UsersService.new
-    service.previous_rounds
-  end
-
-  # def new; end
-  
-  # def create
-  #   new_user = User.new(user_params)
-  #   service = UsersService.new
-
-  #   if user_params[:password] != user_params[:password_confirmation]
-  #     flash[:error] = "Passwords do not match"
-  #     redirect_to "/users/new"
-  #   else
-  #     # It would probably be more efficient to have a post endpoint /users/:email so we don't have an additional handshake here
-  #     user_info = service.get_url("/users/#{new_user.email}")
-  #     service.post_url("/users/#{user_info[:id]}", user_params)
-  #     session[:user_id] = user_info[:id]
-  #     flash[:success] = "Welcome, #{new_user.name}!"
-  #     redirect_to "/rounds/public"
-  #   # else
-  #   #   flash[:error] = new_user.errors.full_messages.to_sentence
-  #   #   redirect_to "/users/new"
-  #   end
-  # end
-
-  # def login_form; end
-
-  # private 
-  
-  # def user_params 
-  #   params.permit(:name, :email, :password, :password_confirmation)
-  # end 
 end
