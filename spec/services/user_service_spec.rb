8fe2f4532d5186e1c077be7c5871e899ec63b884
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-07 14:41:19 -0700

    Add tests to users service specs

--- spec/services/user_service_spec.rb
+++ spec/services/user_service_spec.rb
@@ -1,29 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe UsersService, type: :service do
+  before(:each) do
+    @service = UsersService.new
+  end
+  
   describe 'User Service Instance Methods', :vcr do
     it '#conn, returns a Faraday object' do
-      service = UsersService.new
-
-      expect(service.conn).to be_a(Faraday::Connection)
+      expect(@service.conn).to be_a(Faraday::Connection)
     end
 
     it '#get_url, returns hash data' do
       url = '/users/1/rounds/1'
-      current_round = UsersService.new.get_url(url)
+      current_round = @service.get_url(url)
 
       expect(current_round).to be_a(Faraday::Response)
+      # expect(current_round).to be_a(Hash)
     end
     
     it '#post_url, makes a successful post request' do
       url = '/users/samuelttran@gmail.com'
       info = { email: 'samuelttran@gmail.com' }
-      response = UsersService.new.post_url(url, info)
+      response = @service.post_url(url, info)
 
       expect(response).to be_a(Faraday::Response)
       # expect(response.status) DO WE NEED
     end
 
+    it '#round_id returns a hash with the current round id' do
+      response = @service.round_id
     
+      expect(response).to be_a(Hash)
+      # Add actual data structure
+      # expect(response[:id]).to be_a(Integer)
+    end
   end
 end
