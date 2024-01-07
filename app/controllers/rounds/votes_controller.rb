8f0ecdd4841dc4a30544f3df57bcb53e806d8076
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-07 13:04:58 -0700

    Add get_previous_rounds to votes controller

--- app/controllers/rounds/votes_controller.rb
+++ app/controllers/rounds/votes_controller.rb
@@ -1,4 +1,10 @@
 class Rounds::VotesController < ApplicationController
   def index
+    @previous_rounds_info = get_previous_rounds
+  end
+
+  def get_previous_rounds
+    service = UsersService.new
+    service.get_url("/users/#{session[:user_id]}/recent_rounds")
   end
 end
