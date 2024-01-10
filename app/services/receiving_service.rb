318b04143196325aba23a4c8a6efa364002b5524
Kevin Zolman <kevin.zolman@gmail.com>
2024-01-09 17:44:19 -0700

    Update services

--- app/services/receiving_service.rb
+++ app/services/receiving_service.rb
@@ -1,9 +1,10 @@
 class ReceivingService < WeatherTogetherService 
   def previous_rounds
-    get_response('/users/31/recent_rounds')
+    # where id is the current session id
+    get_response("/users/#{session[:user_id]}/recent_rounds")
   end
 
   def current_round
-    get_response('/users/31/rounds/current_community_round')
+    get_response('/rounds/current_community_round')
   end
 end
